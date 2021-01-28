b00b21b684b47cb77a0b65a09b72e3c526ca6f6e
Fatima-Al-Batesh <fbatesh@gmail.com>
2021-01-28 10:39:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import io from 'socket.io-client';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isConnected:false,
+    old_messages:[],
+    message:{
+      text: null,
+      id:null,
+      name: 'Fatima B'
+    }
+  }
+  socket = null
+
+  componentWillMount(){
+
+    this.socket = io('https://codi-server.herokuapp.com');
+
+    this.socket.on('youare',(answer)=>{
+      this.setState(prevState => ({
+        message: {                   
+            ...prevState.message, 
+            id:answer.id
+          }}
+            ))
+    })
+    // this.socket.on('peeps', clients => this.setState({peeps: clients}))
+    // this.socket.on('new connection', id => this.state.peeps.push(id))
+    // this.socket.on('new disconnection', id => 
+    // this.state.peeps.splice(this.state.peeps.findIndex(function(i){
+    //   return i === id;
+    // }), 1))
+    this.socket.on('next',(message_from_server)=>console.log(message_from_server))
+
+    this.socket.on('connect', () => {
+      this.setState({isConnected:true})
+    })
+    // this.socket.on('pong!',(additionalStuff)=>{
+    //   console.log('server answered!', additionalStuff)
+    // })
+    this.socket.on('disconnect', () => {
+      this.setState({isConnected:false})
+    })
+
+    /** this will be useful way, way later **/
+    this.socket.on('room', old_messages => this.setState({old_messages:old_messages}))
+    // this.socket.on('room', old_messages => console.log(old_messages))
+
+  }
+
+  componentWillUnmount(){
+    this.socket.close()
+    this.socket = null
+  }
+
+  render() {
+    return (<div className="App">
+      <div>status: {this.state.isConnected ? 'connected' : 'disconnected'}</div>
+      {/* add: */}
+      <div>id: {this.state.id}</div>
+      <div>Connected:  </div>
+      {this.state.old_messages.map(item => (
+            <ul>
+            <li key={item}>date:{' ' + item.date + ' '}</li>
+            <li key={item}>name:{' ' +  item.name + ' '} text:{' ' + item.text}</li>
+
+            </ul>
+            ))}
+            {/* <div>number of connections: {this.state.peeps.length}</div>
+      <button onClick={()=>this.socket.emit('ping!')}>ping</button> */}
+      {/* and also add: */}
+      <button onClick={()=>this.socket.emit('whoami')}>Who am I?</button>
+      {/* <button onClick={()=>this.socket.emit("give me next")}>Give me next</button>
+      <button onClick={() => this.socket.emit("hint:addition")}> add </button> */}
+      <input type={String}  onChange={(e)=>{this.setState(prevState => ({
+  message: {                   
+      ...prevState.message,   
+      text:   e.target.value,
+      // id: this.state.id     
+  }
+}))}}></input>
+      <button onClick={()=>this.socket.emit('message', this.state.message)}>Message</button>    
+    </div>);
+  }
 }
 
 export default App;
